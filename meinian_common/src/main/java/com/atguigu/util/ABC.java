29c706275b3dc0eaa6d9b5ac8666d8e6f442be2f
张三硅谷 <31843950+zhangsanatguigu@users.noreply.github.com>
2021-02-02 16:24:29 +0800

    Update ABC.java

--- meinian_common/src/main/java/com/atguigu/util/ABC.java
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -3,11 +3,11 @@ package com.atguigu.util;
 public class ABC {
     public static void main(String[] args) {
         System.out.println("111111111master");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
-        System.out.println("111111111DEV");
+        System.out.println("111111111DEV11");
+        System.out.println("111111111DEV11");
+        System.out.println("111111111DEV11");
+        System.out.println("111111111DEV11");
+        System.out.println("111111111DEV11");
+        System.out.println("111111111DEV11");
     }
 }
